5cef3489c87980e64f1c83443b9388bb8097b282
Masoud Ramezani <masoudhra@gmail.com>
2023-07-18 13:25:28 +1200

    create some class

--- Inheritance/Program.cs
+++ Inheritance/Program.cs
@@ -10,6 +10,63 @@ namespace Inheritance
     {
         static void Main(string[] args)
         {
+            AudioBook book1 = new AudioBook("Animal Farm", "George Orwell", "John");
+            book1.PrintInfo();
+            PdfBook book2 = new PdfBook("Animal Farm", "George Orwell", "John Publication");
+            book2.Price = 21.5;
+            book2.PrintInfo();
+            book2.PrintInfo(book2.Publisher, book2.Price);
+
+            Console.ReadKey();
+
+
+        }
+
+    }
+
+    public class Book
+    {
+        public string Name;
+        public string Author;
+
+        public virtual void PrintInfo() 
+        {
+            Console.WriteLine(Name + "  - " + Author);
+        }
+    }
+
+    public class AudioBook : Book 
+    {
+        public string Narrator;
+
+        public AudioBook(string name, string author, string narrator) 
+        {
+            Name = name;
+            Author = author;
+            Narrator = narrator;
+        }
+
+        public override void PrintInfo()
+        {
+            Console.WriteLine(Name + "  - " + Author + "(" + Narrator + ")");
+        }
+    }
+
+    public class PdfBook : Book 
+    {
+        public string Publisher;
+        public double Price;
+
+        public PdfBook(string name, string author, string publisher)
+        {
+            Name = name;
+            Author = author;
+            Publisher = publisher;
+        }
+
+        public void PrintInfo(string publisher, double price)
+        {
+            Console.WriteLine(Name + "  - " + Author + " - " + publisher + " - " + price + "$");
         }
     }
 }
